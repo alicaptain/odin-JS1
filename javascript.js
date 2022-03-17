3f50960347c71b6ae0e9bc3df9ed3ba97ab8d233
alicaptain <64775480+alicaptain@users.noreply.github.com>
2022-03-17 22:35:51 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log("Hey Mate! This is the 1st time by yourself! (:");
+
+let love = function () {
+  let user = "Aysima";
+  let age = 22;
+  let message = "Seni seviyorum Murat!";
+  return alert(user), alert(age), alert(message);
+};
+
+love();
