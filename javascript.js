3319c826d44db411ca123f633194cd005a6c7501
alicaptain <aligulerworks@gmail.com>
2022-03-20 10:41:21 +0300

    Progress in study

--- javascript.js
+++ javascript.js
@@ -1,10 +1,1 @@
 "use strict";
-
-const button = document.querySelector("button");
-
-function greet() {
-  const name = prompt(`What is your name?`);
-  alert(`Naber ${name}! Iyi ki bastin bana! :)`);
-}
-
-button.addEventListener("click", greet);
